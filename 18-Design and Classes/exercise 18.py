320970f9762e72bf0e31d9adfa46c36294d680e3
ginazheng1028 <ginazheng1028@gmail.com>
2017-03-23 12:54:40 -0400

    car

---
+++ 18-Design and Classes/exercise 18.py
@@ -0,0 +1,30 @@
+
+class Car :
+    def __init__(color,location,direction):
+        self.color=pink
+        self.location=locaiton
+        self.direction=direction
+        
+#set the arguments for color location and direction
+    def set_color(self,color):
+        self.color=color
+    def set_location(self,locaiton):
+        self.location=location
+    def set_direction(self,direction):
+        self.direction
+        
+#the returns for color location and direction        
+    def get_color(self):
+        return self.color
+    def get_location(self):
+        return self.location
+    def get_direction(self):
+        return self.direction
+    
+#create car object
+    def main():
+        my_car=Car()
+        
+        color=print(input('car color: '))
+        locaiton=print(input('car located at  '))
+        direction=print(input('car diretion: ')
